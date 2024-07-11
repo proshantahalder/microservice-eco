6b627bc40041702799817457f62253b0d9f42c4e
Proshanta Halder <p.haider@seu.edu.sa>
2024-07-11 08:52:40 +0300

    New Package Add

--- gatway/index.js
+++ gatway/index.js
@@ -1,18 +1,18 @@
-const express = require('express');
 
+const express = require('express');
+const cors = require('cors');
 const inventoryRoutes = require('../inventory-service');
 const ordersRoutes = require('../order-service');
 
 const app = express();
-
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/inventory', inventoryRoutes);
-app.use('/orders',ordersRoutes);
+app.use('/orders', ordersRoutes);
 
-app.listen(port, () =>{
-    console.log(`API getway listing at http://localhost:${port}`);
-})
-
+app.listen(port, () => {
+  console.log(`API Gateway listening at http://localhost:${port}`);
+});
